2a1d7ee9f9ba2825819f376fcf216d3454a9aca9
Harman Hakim <harman.hakim@ui.ac.id>
2025-09-03 19:24:17 +0700

    feat: add test for main controller

---
+++ src/test/java/io/harman/sidating_app_be/SidatingAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.harman.sidating_app_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidatingAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
