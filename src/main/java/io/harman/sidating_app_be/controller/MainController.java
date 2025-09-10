f9fd908eb892653ccd8f22004e96b01120914bd1
Harman Hakim <harman.hakim@ui.ac.id>
2025-09-10 19:42:19 +0700

    Initial commit

--- src/main/java/io/harman/sidating_app_be/controller/MainController.java
+++ src/main/java/io/harman/sidating_app_be/controller/MainController.java
@@ -7,9 +7,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
-    @GetMapping("/{name}")
-    public String MainController(@PathVariable String name, Model model) {
-        model.addAttribute("name", name);
+    @GetMapping("/{fibonacci}")
+    public String MainController(@PathVariable String fibonacci, Model model) {
+        model.addAttribute("number", fibonacci);
+
+        int n;
+        try{
+            n = Integer.parseInt(fibonacci);
+            if(n<1){
+                model.addAttribute("name", "input bukan bilangan bulat positif");
+            }
+            else{
+                model.addAttribute("name",fibonacci(n));
+            }
+
+        }
+        catch(NumberFormatException e){
+            model.addAttribute("name", "input bukan bilangan bulat"); 
+        }
         return "main";
+    }
+    private int fibonacci(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        return fibonacci(n - 2) + fibonacci(n - 1);
     }
 }
