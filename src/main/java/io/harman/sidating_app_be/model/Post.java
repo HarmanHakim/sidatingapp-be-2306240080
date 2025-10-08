04797b5180e98da7606e5ea226714b8193cc79c5
Harman Hakim <harman.hakim@ui.ac.id>
2025-10-08 22:03:29 +0700

    fix: post model

--- src/main/java/io/harman/sidating_app_be/model/Post.java
+++ src/main/java/io/harman/sidating_app_be/model/Post.java
@@ -7,7 +7,6 @@ import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -30,7 +29,6 @@ import lombok.NoArgsConstructor;
 public class Post {
 
     @Id
-    @GeneratedValue
     private UUID id;
 
     @Column(name = "user_profile_id", nullable = false)
