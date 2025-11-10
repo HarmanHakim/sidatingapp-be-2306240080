75de0870f658e34dc7262a028d6459b66c86ddf1
Harman Hakim <harman.hakim@ui.ac.id>
2025-11-10 17:27:59 +0700

    init

---
+++ src/main/java/io/harman/sidating_app_be/SidatingAppBeApplication.java
@@ -0,0 +1,12 @@
+package io.harman.sidating_app_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidatingAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SidatingAppBeApplication.class, args);
+	}
+}
