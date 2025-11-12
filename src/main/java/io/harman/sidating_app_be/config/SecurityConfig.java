63c80d538bbb0c0a3927b5345e4e917dc9ffbe13
Harman Hakim <harman.hakim@ui.ac.id>
2025-11-12 22:49:44 +0700

    fix: security config

--- src/main/java/io/harman/sidating_app_be/config/SecurityConfig.java
+++ src/main/java/io/harman/sidating_app_be/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
             "http://localhost:5173", 
             "http://localhost:3000", 
-            "http://localhost:8080"
+            "http://localhost:8080",
+            "http://localhost:80"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
