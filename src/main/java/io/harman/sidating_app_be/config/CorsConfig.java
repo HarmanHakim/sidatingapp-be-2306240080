d73a0e0b12f373b4a7276be2d8a2ea340727c927
Harman Hakim <harman.hakim@ui.ac.id>
2025-11-12 21:44:34 +0700

    fix: cors config

--- src/main/java/io/harman/sidating_app_be/config/CorsConfig.java
+++ src/main/java/io/harman/sidating_app_be/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173", "http://localhost:5174")
+						.allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:80")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true)
